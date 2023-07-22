0235235bb32a12cd7c4729ac60ac8b7f44a49363
tvicky7x <tvicky7x@gmail.com>
2023-07-23 01:18:33 +0530

    useRef is used

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,11 @@
+import { useRef } from "react";
+
 function Form(props) {
+  const inputName = useRef();
+  const inputAge = useRef();
   function submitForm(e) {
     e.preventDefault();
-    let name = document.getElementById("name").value;
-    let age = document.getElementById("age").value;
-    let data = { name: name, age: age };
+    let data = { name: inputName.current.value, age: inputAge.current.value };
     props.onSubmit(data);
     e.target.reset();
   }
@@ -12,11 +14,21 @@ function Form(props) {
       <form action="#" onSubmit={submitForm}>
         <div>
           <label className="form-label">Name</label>
-          <input type="text" className="form-control" id="name" />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            ref={inputName}
+          />
         </div>
         <div>
           <label className="form-label">Age</label>
-          <input type="number" className="form-control" id="age" />
+          <input
+            type="number"
+            className="form-control"
+            id="age"
+            ref={inputAge}
+          />
         </div>
         <div className="d-grid mt-3">
           <button className="btn btn-sm btn-dark" type="submit">
