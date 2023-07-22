a8835f0ccca4583d07d9c6c5a207b92470c21ddf
tvicky7x <tvicky7x@gmail.com>
2023-07-23 00:19:10 +0530

    ReactDOM portal added

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,39 +1,46 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import styles from "./Model.module.css";
 
+let errorMassage = [
+  { title: "Invalid Input", massage: "Please add valid inputs" },
+  {
+    title: "Invalid Age",
+    massage: "Please make sure your age is above 1",
+  },
+];
+
 function Modal(props) {
-  let errorMassage = [
-    { title: "Invalid Input", massage: "Please add valid inputs" },
-    {
-      title: "Invalid Age",
-      massage: "Please make sure your age is above 1",
-    },
-  ];
   function closeModal() {
     props.onClick();
   }
   return (
     <>
-      <div className={styles.shadBackground}>
-        <div
-          className="h-100 d-flex justify-content-center align-items-center"
-          onClick={closeModal}
-        >
-          <div className={`container card p-0 ${styles.size}`}>
-            <div className="card-header">
-              <h3>{errorMassage[props.MassageIndex].title}</h3>
-            </div>
-            <div className="card-body">
-              <p>{errorMassage[props.MassageIndex].massage}</p>
-            </div>
-            <div className="card-footer">
-              <button className="btn btn-dark btn-sm" onClick={closeModal}>
-                Close
-              </button>
+      {ReactDOM.createPortal(
+        <>
+          <div className={styles.shadBackground}>
+            <div
+              className="h-100 d-flex justify-content-center align-items-center"
+              onClick={closeModal}
+            >
+              <div className={`container card p-0 ${styles.size}`}>
+                <div className="card-header">
+                  <h3>{errorMassage[props.MassageIndex].title}</h3>
+                </div>
+                <div className="card-body">
+                  <p>{errorMassage[props.MassageIndex].massage}</p>
+                </div>
+                <div className="card-footer">
+                  <button className="btn btn-dark btn-sm" onClick={closeModal}>
+                    Close
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>,
+        document.getElementById("modal--root")
+      )}
     </>
   );
 }
